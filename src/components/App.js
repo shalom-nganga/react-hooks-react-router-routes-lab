4400be49acb68e7677260abf02d35c075a3c2f9f
shalom-nganga <minnomacharia@gmail.com>
2023-11-27 10:09:03 +0300

    assignments done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Movies from './Movies';
+import Directors from './Directors';
+import Actors from './Actors';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
